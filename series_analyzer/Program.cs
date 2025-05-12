12daa460794826d12279cdf991be0bce307c07b5
moshik <moshik0527669169@gmail.com>
2025-05-12 17:37:09 +0300

    printList , printRevers , sortList

--- series_analyzer/Program.cs
+++ series_analyzer/Program.cs
@@ -10,15 +10,14 @@ namespace series_analyzer
         {
             menu();
 
+            List<int> numbers = new List<int>();
             void menu()
             {
-
-                Console.WriteLine("enter series numbers");
-                string series = Console.ReadLine();
-                bool temp = false;
+                seriesUpdate();
+                bool temp = true;
                 while (temp)
                     {                
-                    Console.WriteLine("For replace a series of numbers clik 0 and for Continue with existing number clik 1\n" +
+                    Console.WriteLine("For replace a series of numbers clik 1\n" +
                         "To print the serues numbers clik 2\n" +
                         "To print the series in reverse clik 3\n" +
                         "To Sort the series clik 4\n" +
@@ -32,17 +31,17 @@ namespace series_analyzer
                     switch (choice)
                     {
                         case 1:
-                            seriesUpdate();
+                           numbers = seriesUpdate();
                             break;
-                        //case 2:
-                        //    printSeries(series);
-                        //    break;
-                        //case 3:
-                        //    printReverse(series);
-                        //    break;
-                        //case 4:
-                        //    sortSeries(series);
-                        //    break;
+                        case 2:
+                            printSeries(numbers);
+                            break;
+                        case 3:
+                            printReverse(numbers);
+                            break;
+                        case 4:
+                            sortSeries(numbers);
+                            break;
                         //case 5:
                         //    maximum(series);
                         //    break;
@@ -77,9 +76,47 @@ namespace series_analyzer
                     int num = int.Parse(number);
                     seriesNumbers.Add(num);                 
                 }
-                Console.WriteLine("D");
+                numbers = seriesNumbers;
                 return seriesNumbers;
             }
+            void printSeries(List<int> numbersList)
+            {
+                foreach (var number in numbersList)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            void printReverse(List<int> numbersList)
+            {
+                for (int i = (numbersList.Count)-1;i > -1;i--)
+                {
+                    Console.WriteLine(numbersList[i]);
+                }
+            }
+            void sortSeries(List<int> numbersList)
+            {
+                List<int> sortList = new List<int>();
+                List<int> tempList = numbersList;
+                while (tempList.Count != 0)
+                {
+                    int minimum = numbersList[0];
+                    foreach (var number in numbersList)
+                    {
+                        if (number < minimum)
+                        {
+                            minimum = number;
+                        }
+                    }
+                    sortList.Add(minimum);
+                    numbersList.Remove(minimum);
+                }
+                foreach (var number in sortList)
+                {
+                    Console.WriteLine(number);
+                }
+
+            }
+
 
         }
     }
